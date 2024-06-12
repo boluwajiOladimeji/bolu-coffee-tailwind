8a7022864590cdf1d0fa3d0b56953e6d8929fc51
boluwajiOladimeji <oladimejiboluwaji@gmail.com>
2024-06-12 13:05:03 +0100

    new

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const openSidebarBtn = document.querySelector('.open-btn');
 const closeSidebarBtn = document.querySelector('.close-btn');
 const sidebarEl = document.querySelector('.sidebar');
 const navBtns = document.querySelector('.nav-btns');
+const sidebarBtnsEl = document.querySelectorAll('.side-nav');
 
 openSidebarBtn.addEventListener('click', () => {
   sidebarEl.classList.add('show-sidebar');
@@ -11,3 +12,10 @@ closeSidebarBtn.addEventListener('click', () => {
   sidebarEl.classList.remove('show-sidebar');
   navBtns.classList.remove('sidebar-open');
 });
+
+sidebarBtnsEl.forEach((btn) =>
+  btn.addEventListener('click', () => {
+    navBtns.classList.remove('sidebar-open');
+    sidebarEl.classList.remove('show-sidebar');
+  })
+);
